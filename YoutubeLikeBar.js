f0a7da62a95f250b58742b01ad9b966d48e24104
Maxime Cyr <max_maxime_max@hotmail.com>
2013-05-29 17:27:04 -0400

    Correction d'un bug qui rendait le bookmarklet incompatible avec Firefox

--- YoutubeLikeBar.js
+++ YoutubeLikeBar.js
@@ -1,26 +1,3 @@
-var Videos = document.getElementsByClassName('related-video');
-for(var i = 0 ; i < Videos.length ; i++)
-{
-	var Elem = Videos[i];
-	var URL = Elem.getAttribute('href');
-	var URLBegin = '/watch?v=';
-	var VideoID = URL.substr(URLBegin.length);
-	Request(VideoID, Elem, function(rep, Elem){
-		var StatsElem = rep.getElementsByTagName('rating')[1];
-		var LikeCount = parseInt(StatsElem.getAttribute('numLikes'));
-		var DislikeCount = parseInt(StatsElem.getAttribute('numDislikes'));
-		var TotalRateCount = LikeCount + DislikeCount;
-		var LikePourcent = Math.round(LikeCount * 100 / TotalRateCount);
-		var DislikePourcent = 100 - LikePourcent;
-
-		var NewElem = document.createElement('span');
-
-		NewElem.innerHTML += '<span style="height: 3px; float: left; background-color: green; width: '+LikePourcent+'%;"></span>';
-		NewElem.innerHTML += '<span style="height: 3px; float: left; background-color: red; width: '+DislikePourcent+'%;"></span>';
-		Elem.appendChild(NewElem);
-	});
-}
-
 function getXMLHttpRequest()
 {
     var xhr = null;
@@ -48,10 +25,38 @@ function Request(VideoID, Elem, callback)
 
 	xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-            callback(xhr.responseXML, Elem);
+            callback(JSON.parse(xhr.responseText), Elem);
         }
     };
 
-    xhr.open("GET", "https://gdata.youtube.com/feeds/api/videos/"+VideoID+"?v=2", true);
+    var URL = "https://gdata.youtube.com/feeds/api/videos/"+VideoID+"?v=2&alt=json";
+    console.log(URL);
+    xhr.open("GET", URL, true);
 	xhr.send(null);
 }
+
+var Videos = document.getElementsByClassName('related-video');
+for(var i = 0 ; i < Videos.length ; i++)
+{
+	var Elem = Videos[i];
+	var URL = Elem.getAttribute('href');
+	var URLBegin = '/watch?v=';
+	var VideoID = URL.substr(URLBegin.length);
+	Request(VideoID, Elem, function(rep, Elem){
+		if(rep == null) return;
+
+		var StatsElem = rep['entry']['yt$rating'];
+		var LikeCount = parseInt(StatsElem['numLikes']);
+		var DislikeCount = parseInt(StatsElem['numDislikes']);
+		var TotalRateCount = LikeCount + DislikeCount;
+		var LikePourcent = Math.round(LikeCount * 100 / TotalRateCount);
+		var DislikePourcent = 100 - LikePourcent;
+
+		var NewElem = document.createElement('span');
+
+		NewElem.innerHTML += '<span style="height: 3px; float: left; background-color: green; width: '+LikePourcent+'%;"></span>';
+		NewElem.innerHTML += '<span style="height: 3px; float: left; background-color: red; width: '+DislikePourcent+'%;"></span>';
+		Elem.appendChild(NewElem);
+	});
+}
+
